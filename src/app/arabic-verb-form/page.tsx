015dd5d4b547c78d316f4b6968aaf80c89c7fb91
tautastic <tautastic@proton.me>
2026-09-15 19:26:08 +0200

    Fix build time rendering bug

--- src/app/arabic-verb-form/page.tsx
+++ src/app/arabic-verb-form/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { formatMorphPatternFormNumber } from "~/lib/formatting";
 import { getVerbFormsWithCounts } from "~/server/db/repository/morph-pattern";
 
+export const dynamic = "force-dynamic";
+
 export default async function ArabicVerbFormsPage() {
   const forms = await getVerbFormsWithCounts();
 
